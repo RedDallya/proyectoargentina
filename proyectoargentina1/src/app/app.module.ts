ef108200e946eaba7a7b513a30eb8e8e453945c6
pia salas <elbosquecito30@gmail.com>
2023-01-26 16:52:25 -0300

    subida de componentes

--- proyectoargentina1/src/app/app.module.ts
+++ proyectoargentina1/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { LogoargComponent } from './components/logoarg/logoarg.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    LogoargComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
